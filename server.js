d1533feda4a861a21f3f82b6594e8d03d6a7fc6c
dribble-njr <wzw15292257101@163.com>
2024-02-10 17:14:06 +0800

    feat: based on event callback

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from "http";
 
 http
   .createServer((request, response) => {
+    console.log("Request received.");
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.write("Hello World");
     response.end();
