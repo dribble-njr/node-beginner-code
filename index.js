813b6c2933efaf7d17663b1369f06e91b3416a82
dribble-njr <wzw15292257101@163.com>
2024-02-11 14:12:39 +0800

    feat: route for the real requestHandlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import server from "./server.js";
 import router from "./router.js";
+import requestHandlers from "./requestHandlers.js";
 
-server.start(router.route);
+const handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import http from "node:http";
 import { URL } from "node:url";
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     const url = new URL(request.url, `http://${request.headers.host}`);
     const pathname = url.pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.write("Hello World");
