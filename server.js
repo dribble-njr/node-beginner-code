6dc1b8e76ad6bfdd9c828d5da0e7bec01c725ebe
dribble-njr <wzw15292257101@163.com>
2024-02-13 19:44:30 +0800

    feat: handle post data

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ import { URL } from "node:url";
 
 function start(route, handle) {
   function onRequest(request, response) {
+    let postData = "";
     const url = new URL(request.url, `http://${request.headers.host}`);
     const pathname = url.pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", (postDataChunk) => {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end", () => {
+      route(handle, pathname, response, postData);
+    });
   }
 
   http.createServer(onRequest).listen(8888);
