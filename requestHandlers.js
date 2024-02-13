e0ae98280ad618038486f3afc689177de4b3571e
dribble-njr <wzw15292257101@163.com>
2024-02-13 20:15:09 +0800

    feat: select a local image and show it

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,7 @@
-import querystring from "node:querystring";
 import fs from "node:fs";
+import formidable from "formidable";
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called.");
 
   const body = `
@@ -10,9 +10,9 @@ function start(response, postData) {
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       </head>
       <body>
-        <form action="/upload" method="post">
-          <textarea name="text" rows="20" cols="60"></textarea>
-          <input type="submit" value="Submit text" />
+        <form action="/upload" method="post" enctype='multipart/form-data'>
+        <input type="file" name="upload" />
+        <input type="submit" value="Upload file" />
         </form>
       </body>
     </html>
@@ -23,11 +23,23 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("You've sent: " + querystring.parse(postData).text);
-  response.end();
+
+  const form = formidable({});
+  console.log("about to parse");
+
+  form.parse(request, (error, fields, files) => {
+    console.log("parsing done", files);
+
+    const uploadedFile = files.upload[0];
+    fs.renameSync(uploadedFile.filepath, "./tmp/test.jpeg");
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
 }
 
 function show(response) {
